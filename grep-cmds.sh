c142c67365a66c56a3cd0c5f324e8a3d10a5b04a
sinkovit <sinkovit@sdsc.edu>
2025-04-07 10:27:14 -0700

    Adds scripts and text files to repo

---
+++ grep-cmds.sh
@@ -0,0 +1,14 @@
+grep banana file1.txt
+grep Banana file1.txt
+grep -i banana file1.txt
+grep -B 3 kumquat file1.txt
+grep -A 2 kumquat file1.txt
+grep lime *.txt
+grep -v lime *.txt
+grep lime *.txt
+grep -c lime *.txt
+grep -l kumquat *.txt
+grep -L kumquat *.txt
+grep pear file1.txt
+grep '^pear' file1.txt
+grep 'pear$' file1.txt
